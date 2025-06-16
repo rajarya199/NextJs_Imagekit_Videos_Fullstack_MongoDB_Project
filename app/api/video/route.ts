6773ff3e532e7ac37554d98c1eb936f4d38831fb
rajarya199 <aryalrajan078@gmail.com>
2025-06-16 18:27:24 +0545

    post video

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,8 @@
+import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import Video from "@/models/Video";
-import { NextResponse } from "next/server";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 export async function GET(){
     try{
         await connectToDatabase()
@@ -17,5 +19,53 @@ export async function GET(){
             {status:500}
         )
 
+    }
+}
+
+export async function POST(request:NextRequest){
+    try{
+        //check if the user is authenticated
+const session=await getServerSession(authOptions)
+if(!session){
+       console.error("Error fetching videos:");
+        return NextResponse.json(
+            { error:"unauthorized user" },
+            {status:401}
+        )
+}
+//check db conneted
+await connectToDatabase()
+//post
+const body:IVideo=await request.json()
+if(!body.title ||
+    !body.description || 
+    !body.videoUrl ||
+     !body.thumbnailUrl
+){
+    return NextResponse.json(
+        { error: "missing required filed" },
+        { status: 400 }
+    );
+}
+
+//create video data to post
+const videoData={
+    ...body,
+    controls: body?.controls ?? true,
+    transformations:{
+        height:1920,
+        width:1080,
+        quality:body.transformation?.quality ?? 100
+    }
+}
+const newVideo =await Video.create(videoData)
+return NextResponse.json(newVideo, { status: 201 });
+    }
+    catch(error){
+        console.error("Error uploading video:", error);
+        return NextResponse.json(
+            { error: "Failed to upload video" },
+            { status: 500 }
+        );
     }
 }
