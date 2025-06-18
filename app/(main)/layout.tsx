761db07239666d778ade86cc689fbfdd78c86a41
rajarya199 <aryalrajan078@gmail.com>
2025-06-18 17:57:44 +0545

    main layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Layout=async({children}:{children:React.ReactNode})=>{
+    return (
+         <main className="w-full min-h-screen bg-gray-50">
+      <Navbar />
+     {children}
+    </main>
+
+    )
+}
+ export default Layout
