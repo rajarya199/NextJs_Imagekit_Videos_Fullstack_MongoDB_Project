4bc100fb52f1cec40433a7e3d307c9508c07cb91
rajarya199 <aryalrajan078@gmail.com>
2025-06-19 21:01:07 +0545

    menu items

---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+export const menuItem=[
+    {
+        label:'Home',
+        path:'/'
+    },
+     {
+        label:"Features",
+        path:"/"
+    },
+     {
+        label:"Pricing",
+        path:"/"
+    },
+     {
+        label:"About",
+        path:"/"
+    },
+
+]
