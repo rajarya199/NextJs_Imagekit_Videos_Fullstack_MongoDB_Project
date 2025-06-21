0655eaf51da2e834524f36fbdad3134c4e90d8ef
rajarya199 <aryalrajan078@gmail.com>
2025-06-21 23:20:16 +0545

    dynamic import

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -1,5 +1,13 @@
+"use client"
 import { SessionProvider } from "next-auth/react"
-import { ImageKitProvider } from "@imagekit/next"
+import dynamic from "next/dynamic";
+
+// 1️⃣  Dynamically import *only* the named export we need
+const ImageKitProvider = dynamic(
+  () =>
+    import("@imagekit/next").then((mod) => mod.ImageKitProvider),
+  { ssr: false }          // makes sure it never runs in SSR / RSC
+);
 const urlEndPoint=process.env.NEXT_PUBLIC_URL_ENDPOINT!
 export default function Provider({children}:{children:React.ReactNode}){
     return <SessionProvider refetchInterval={5*60}>
