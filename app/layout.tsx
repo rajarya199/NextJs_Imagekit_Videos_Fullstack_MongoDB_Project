b30ff868d08e0eb10a3e7cc85035e843a4e02d4c
rajarya199 <aryalrajan078@gmail.com>
2025-06-21 19:54:13 +0545

    provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-
+import Provider from "./components/Provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,10 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-
-        {children}
-        </ThemeProvider>
+     <Provider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );
