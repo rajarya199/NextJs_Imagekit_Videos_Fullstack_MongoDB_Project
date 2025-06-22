50a77b2a8a13b53e76a6ebe1c126f337d7a825fa
rajarya199 <aryalrajan078@gmail.com>
2025-06-22 23:45:11 +0545

    gallery

---
+++ app/(main)/gallery/page.tsx
@@ -0,0 +1,12 @@
+import VideoGallery from '@/components/gallery/VideoGallery'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='wrapper'>
+      <VideoGallery/>
+    </div>
+  )
+}
+
+export default page
