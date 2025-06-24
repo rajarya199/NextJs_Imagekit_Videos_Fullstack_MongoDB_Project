a92b6f6861efcc78edba4d76b9a169fbb65f4aa2
rajarya199 <aryalrajan078@gmail.com>
2025-06-24 17:59:36 +0545

    video card

--- components/gallery/VideoCard.tsx
+++ components/gallery/VideoCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client"
+import React,{useState} from 'react';
+import Link from 'next/link';
 interface Video {
   _id: string;
   title: string;
@@ -18,22 +20,52 @@ video:Video
 const VideoCard: React.FC<VideoCardProps> = ({
  video
 }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
-      <video
+    <Link href={`/video/${video._id}`} passHref>
+      <div className="dark:bg-primary-600 border border-gray-100 dark:border-gray-700 rounded-lg shadow-md overflow-hidden flex flex-col cursor-pointer transition-transform duration-200 hover:scale-105"
+    
+     onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className='relative w-full h-[250px]'>
+        {!isHovered ? (
+           <img
+            src={video.thumbnailUrl}
+            alt={video.title}
+            className="w-full h-full object-cover"
+            loading="lazy"
+          />
+        ):(
+           <video
+            src={video.videoUrl}
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="w-full h-full object-cover"
+          />
+        )}
+
+      </div>
+      {/* <video
         src={video.videoUrl}
             poster={video.thumbnailUrl}
             controls={video.controls ?? true}
             width={video.transformation?.width}
             height={video.transformation?.height}
         className="w-full h-[200px] object-cover"
-      />
+      /> */}
       <div className="p-4 flex flex-col flex-grow">
-        <h3 className="text-lg font-semibold mb-2 text-gray-900">{video.title}</h3>
-        <p className="text-gray-600 flex-grow">{video.description}</p>
+        <h3 className="text-lg font-medium line-clamp-2 mb-2 text-gray-900 dark:text-gray-200">{video.title}</h3>
+        {/* <p className="text-gray-600 flex-grow ">{video.description}</p> */}
       </div>
     </div>
-  );
+  
+    </Link>
+  )
+  
 };
 
 export default VideoCard;
