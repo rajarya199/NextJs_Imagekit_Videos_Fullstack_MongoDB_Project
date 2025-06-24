69c495cbed44d4725397fe5f283575c5f4eaae93
rajarya199 <aryalrajan078@gmail.com>
2025-06-24 14:20:57 +0545

    get video by id

---
+++ app/api/video/[id]/route.ts
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "@/lib/db";
+import Video from "@/models/Video";
+import { NextResponse } from "next/server";
+
+export async function GET(
+     request: Request,
+  { params }: { params: { id: string } }
+){
+try{
+    await connectToDatabase()
+        const { id } = params;
+  if (!id) {
+      return NextResponse.json(
+        { error: "Video ID is required" },
+        { status: 400 }
+      );
+    }
+    const video=await Video.findById(id).lean()
+     if (!video) {
+      return NextResponse.json(
+        { error: "Video not found" },
+        { status: 404 }
+      );
+    }
+        return NextResponse.json(video, { status: 200 });
+
+}
+catch(error){
+     console.error("Error fetching video by ID:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch video" },
+      { status: 500 }
+    );
+}
+}
