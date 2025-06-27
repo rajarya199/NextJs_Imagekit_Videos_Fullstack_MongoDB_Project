2af5c14fe340dc3b6ca6d748b1eca1464a0049e7
rajarya199 <aryalrajan078@gmail.com>
2025-06-27 23:31:27 +0545

    img config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,18 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
+        {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
+      },
+        {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
